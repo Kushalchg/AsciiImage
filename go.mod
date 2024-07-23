8865f1844073c742ce6844f1a3e9f6c2df72c66b
kushal chapagain <chapagainkushal74@gmail.com>
2024-07-23 23:14:26 +0545

    added resize image and upscale and sownscale the image

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module www.github.com/kushalchg/AsciiImage
 
 go 1.22.3
+
+require (
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	golang.org/x/image v0.18.0
+)
--- main.go
+++ main.go
@@ -5,17 +5,21 @@ import (
 	"image"
 	"image/png"
 	"os"
+
+	"golang.org/x/image/draw"
 )
 
 func main() {
-	// image := LoadImage()
-	// image := CreateSampleImage()
-	// file, err := os.Create("logo.png")
-	// if err != nil {
-	// 	fmt.Printf("error while opening file %v\n", err)
+	image := LoadImage()
+	resizeImage := ResizeImage(image, 3000)
 
-	// }
-	// png.Encode(file, image)
+	file, err := os.Create("logo.png")
+	if err != nil {
+		fmt.Printf("error while opening file %v\n", err)
+
+	}
+	defer file.Close()
+	png.Encode(file, resizeImage)
 
 }
 func LoadImage() image.Image {
@@ -36,5 +40,18 @@ func LoadImage() image.Image {
 }
 
 // func ResizeImage(img image.Image, width int) image.Image {
+// 	bound := img.Bounds()
+// 	height := (bound.Dy() * width) / bound.Dx()
+// 	newImage := image.NewRGBA(image.Rect(0, 0, height, width))
+// 	draw.Draw(newImage, newImage.Bounds(), img, img.Bounds().Size(), draw.Src)
+// 	return newImage
 
 // }
+func ResizeImage(img image.Image, width int) image.Image {
+	bounds := img.Bounds()
+	height := (bounds.Dy() * width) / bounds.Dx()
+
+	newImage := image.NewRGBA(image.Rect(0, 0, width, height))
+	draw.CatmullRom.Scale(newImage, newImage.Bounds(), img, bounds, draw.Over, nil)
+	return newImage
+}
